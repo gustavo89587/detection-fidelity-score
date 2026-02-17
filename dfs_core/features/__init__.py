c0263f9f51d5ff8929dbfc8a96ec8bd0e2252ec6
Samurai Blue <161786778+gustavo89587@users.noreply.github.com>
2026-02-17 13:37:32 -0300

    Update __init__.py

--- dfs_core/features/__init__.py
+++ dfs_core/features/__init__.py
@@ -2,3 +2,4 @@
 # Import feature modules so they self-register.
 from dfs_core.features import windows_4688  # noqa: F401
 from dfs_core.features import aws_cloudtrail_iam  # noqa: F401
+from dfs_core.features import windows_sysmon_1  # noqa: F40
