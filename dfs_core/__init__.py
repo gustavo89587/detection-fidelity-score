f37ca585e1ca31d8f15231df5ea6fa46fc59b7b2
Gustavo Okamoto <gugaokamoto1@gmail.com>
2026-03-05 18:59:55 -0300

    fix: add DFSModel to dfs_core

--- dfs_core/__init__.py
+++ dfs_core/__init__.py
@@ -1,2 +1,11 @@
 from .guardrails import DFSGuardrail, DFSAction
 from .scoring import calculate_score, classify, clamp01, DFSInputs
+
+
+class DFSModel:
+    def score(self, inputs) -> float:
+        if isinstance(inputs, tuple):
+            s, t, b = inputs
+        else:
+            s, t, b = inputs.s, inputs.t, inputs.b
+        return round(s * t * b, 4)
